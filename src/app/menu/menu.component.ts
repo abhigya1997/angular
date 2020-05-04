a3aa5adf3a0f840bd2a3c141f8d579bce89cdb64
abhigya1997 <abhibhar1697@gmail.com>
2020-05-05 02:38:18 +0530

    assignment 1

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
 
-DISHES: Dish[] = [
+const DISHES: Dish[] = [
     {
       id: '0',
       name: 'Uthappizza',
