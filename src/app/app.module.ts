2841f6df127b98318be58290a5e833a7fce43137
abhigya1997 <abhibhar1697@gmail.com>
2020-05-07 18:11:27 +0530

    SPA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,7 @@ import { MenuComponent } from './menu/menu.component';
 import {MatListModule} from '@angular/material/list';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
-import { DishService } from './services/dish.service';
+
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
@@ -26,6 +26,8 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
+import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 
 
 @NgModule({
