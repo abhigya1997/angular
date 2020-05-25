f2c9a124b897f8316569cfa25e6f33c90f559b4a
abhigya1997 <abhibhar1697@gmail.com>
2020-05-25 19:14:19 +0530

    reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms'; 
-
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
@@ -63,9 +65,12 @@ import { LoginComponent } from './login/login.component';
      MatSidenavModule,
      MatDialogModule,
      MatFormFieldModule, 
+     MatSelectModule,
+    MatSlideToggleModule,
     MatInputModule,
     MatCheckboxModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   entryComponents: [
         LoginComponent
